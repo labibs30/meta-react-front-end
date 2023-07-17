a1e7cc44240f17b4d7027dce254d3a0ec9a48d40
labib3013 <alfarabyhd30@gmail.com>
2023-07-17 23:49:39 +0700

    feat : add a new component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
+function Heading() {
+  let title = "This is some heading text";
+  return <h1>{title}</h1>;
+}
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Heading />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
