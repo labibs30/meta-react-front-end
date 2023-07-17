5a93ee9a72738b28cd5c211d244c8b5f24c648ce
labib3013 <alfarabyhd30@gmail.com>
2023-07-18 00:13:47 +0700

    feat add

---
+++ src/components/Nav.js
@@ -0,0 +1,14 @@
+function Nav() {
+  return (
+    <nav>
+      <ul className="main-nav">
+        <li>Home</li>
+        <li>Articles</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
---
+++ src/components/Promo.js
@@ -0,0 +1,14 @@
+function Promo() {
+  return (
+    <div className="promo-section">
+      <div>
+        <h1>Don't miss this deal!</h1>
+      </div>
+      <div>
+        Subscribe to our newsletter and get 20% off your first purchase!
+      </div>
+    </div>
+  );
+}
+
+export default Promo;
