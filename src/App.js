f4ee0d2874e9eaa019837487a4c49ca528f17b1d
labib3013 <alfarabyhd30@gmail.com>
2023-07-18 00:20:07 +0700

    feat :add everything

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Heading from "./Heading";
 
-function Heading() {
-  let title = "This is some heading text";
-  return <h1>{title}</h1>;
-}
 function Header() {
   return <h1>Hello World</h1>;
 }
 function App() {
-  return <Header />;
+  return (
+    <div>
+      <Heading />
+      <Header />
+    </div>
+  );
 }
 
 export default App;
